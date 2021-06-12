2ce70f6623a2e7ce308c92ea9ccc103efc9a89f0
ParallelScore 2015 - Azeez <a.oriaje@parallelscoregit>
2021-06-12 14:49:56 +0100

    added project setup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,25 +5,23 @@ import "./index.less";
 
 const AppHeader = () => {
   return (
-    <div id="app-header">
-      <Navbar fixed="top" expand="lg">
-        <Navbar.Brand href="#home">Mony</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#services">Service</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Sign Up</Nav.Link>
-            <Nav.Link href="#memes">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Navbar fixed="top" id="app-header" expand="lg">
+      <Navbar.Brand href="/">Mony</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#services">Service</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link className="sign-up-link ">Sign Up</Nav.Link>
+          <Nav.Link className="login-link">Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
